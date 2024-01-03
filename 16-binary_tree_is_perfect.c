1105af38290e14c26a1c4c4ee0e43a12914b90d5
FairTee <stephenodjidja7@gmail.com>
2024-01-03 07:03:16 -0800

    Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,21 +6,21 @@
  *
  * Return: 1 if the tree is perfect, 0 otherwise
  */
+
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t left_h, right_h;
-
 	if (tree == NULL)
 		return (0);
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			return (1);
 
-	/* If the tree is not full, it can't be perfect */
-	if (left_h != right_h)
-		return (0);
+		if (tree->left && tree->right)
+			return (binary_tree_is_perfect(tree->left) &&
+					binary_tree_is_perfect(tree->right));
+	}
 
-	/* Recursively check if the left and right subtrees are perfect */
-	return (binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right));
+	return (0);
 }
